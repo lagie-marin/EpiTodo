829661e59bbe93e3ef26620c68677b06a601d4b7
lagie-marin <marin.lagie@epitech.eu>
2024-05-07 13:18:15 +0200

    [UPDATE]Put the return in the auth.js file

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -2,10 +2,12 @@ const jwt = require('jsonwebtoken');
 
 module.exports = function (req, res, next) {
     const headersauth = req.headers['authorization'];
-    if (!headersauth) return res.status(401).json({"msg":"No token, acces denied"});
+    if (!headersauth)
+        return res.status(401).json({"msg":"No token, acces denied"});
     const token = headersauth.split(' ')[1];
     jwt.verify(token, process.env.SECRET, (err, result) => {
-        if (err) return res.status(401).json({"msg":"Token not valid"});
+        if (err)
+            return res.status(401).json({"msg":"Token not valid"});
         req.iduser = result["id"];
         next();
     });
