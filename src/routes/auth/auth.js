93e7fe5516b93e90a4f4855cf10d97dd49ccaecd
lagie-marin <marin.lagie@epitech.eu>
2024-05-14 10:16:38 +0200

    [UPDATE]check if email, name, firstname and password is good

--- src/routes/auth/auth.js
+++ src/routes/auth/auth.js
@@ -1,5 +1,6 @@
-const { logs } = require('../../utils/Logger');
+const { validate } = require('email-validator');
 const { checkAccountMail, getAccountMail, register } = require('../user/user.query');
+const { logs } = require('../../utils/Logger');
 
 module.exports = function (app, bcrypt) {
     app.post("/login", (req, res) => {
@@ -22,11 +23,23 @@ module.exports = function (app, bcrypt) {
         var fn = req.body["firstname"];
         var mdp = req.body["password"];
 
-        if (mail == undefined || name == undefined || fn == undefined || mdp == undefined ||
-            mail.length == 0 || name.length == 0 || fn.length == 0)
+        if (mail == undefined || name == undefined || fn == undefined || mdp == undefined)
             res.status(400).json({"msg":"there is information missing"});
+        else if (mail.length == 0 || name.length == 0 || fn.length == 0)
+            res.status(400).json({"msg": "email, name and firstname is required"});
+        else if (!validate(mail))
+            res.status(400).json({"msg": "Email is not valid"});
+        else if (name == fn)
+            res.status(400).json({"msg": "Name and surname must be different"});
+        else if (name == mdp || fn == mdp || mail == mdp)
+            res.status(400).json({"msg": "The password must be different from the name, first name and email"});
+        else if (mdp.length < 8)
+            res.status(400).json({"msg": "Password must contain at least 8 characters"});
+        else if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W])[A-Za-z\d\W]{8,}$/.test(mdp))
+            res.status(400).json({"msg": "Password must contain lower case, upper case, number and special characters"});
         else {
             mdp = bcrypt.hashSync(mdp, 10);
+            logs(`mdp: ${mdp}`);
             checkAccountMail(res, mail, nb => {
                 if (nb == 84)
                     res.status(403).json({ "msg": "Account already exists" });
