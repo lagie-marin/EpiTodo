81422c4cc0e0e78d97b0be7e349bf878c4f97e3f
lagie-marin <marin.lagie@epitech.eu>
2024-05-14 10:39:12 +0200

    [UPDATE]delete the log when the person connects

--- src/routes/auth/auth.js
+++ src/routes/auth/auth.js
@@ -13,7 +13,7 @@ module.exports = function (app, bcrypt) {
         }
         getAccountMail(res, mail, mdp, bcrypt, (nb) => {
             if (nb == 84)
-                res.status(401).json({"msg" : "Invalid Credentials"});
+                res.status(403).json({"msg" : "Invalid Credentials"});
         });
     });
 
@@ -25,7 +25,7 @@ module.exports = function (app, bcrypt) {
 
         if (mail == undefined || name == undefined || fn == undefined || mdp == undefined)
             res.status(400).json({"msg":"there is information missing"});
-        else if (mail.length == 0 || name.length == 0 || fn.length == 0)
+        else if (mail.length === 0 || name.length === 0 || fn.length === 0)
             res.status(400).json({"msg": "email, name and firstname is required"});
         else if (!validate(mail))
             res.status(400).json({"msg": "Email is not valid"});
@@ -39,7 +39,6 @@ module.exports = function (app, bcrypt) {
             res.status(400).json({"msg": "Password must contain lower case, upper case, number and special characters"});
         else {
             mdp = bcrypt.hashSync(mdp, 10);
-            logs(`mdp: ${mdp}`);
             checkAccountMail(res, mail, nb => {
                 if (nb == 84)
                     res.status(403).json({ "msg": "Account already exists" });
